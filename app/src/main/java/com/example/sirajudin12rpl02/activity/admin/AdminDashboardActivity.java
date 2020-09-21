4c69ba6995838f00aa6ff7f4ebe2db9f21339e5d
SirajudinSalam <ainuryazid64@gmail.com>
2020-09-21 11:14:47 +0700

    update-fix

--- app/src/main/java/com/example/sirajudin12rpl02/activity/admin/AdminDashboardActivity.java
+++ app/src/main/java/com/example/sirajudin12rpl02/activity/admin/AdminDashboardActivity.java
@@ -1,21 +1,33 @@
 package com.example.sirajudin12rpl02.activity.admin;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.sirajudin12rpl02.R;
+import com.example.sirajudin12rpl02.helper.Config;
 
 public class AdminDashboardActivity extends AppCompatActivity {
     private RelativeLayout div_Sepeda, div_Customer;
+    private TextView tv_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+        tv_logout = findViewById(R.id.logout);
+        tv_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
         div_Sepeda = findViewById(R.id.daftarsepeda);
 //        div_Sepeda.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -37,5 +49,20 @@ public class AdminDashboardActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         finish();
+    }
+    private void logout() {
+        new AlertDialog.Builder(AdminDashboardActivity.this)
+                .setTitle("Logout")
+                .setMessage("Anda yakin akan logout ?")
+                .setNegativeButton("Tidak", null)
+                .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    private void doNothing() {
+
+                    }
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Config.forceLogout(AdminDashboardActivity.this);
+                    }
+                }).create().show();
     }
 }
--- app/src/main/java/com/example/sirajudin12rpl02/activity/customer/DashboardActivity.java
+++ app/src/main/java/com/example/sirajudin12rpl02/activity/customer/DashboardActivity.java
@@ -1,21 +1,33 @@
 package com.example.sirajudin12rpl02.activity.customer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.sirajudin12rpl02.R;
+import com.example.sirajudin12rpl02.helper.Config;
 
 public class DashboardActivity extends AppCompatActivity {
     private RelativeLayout div_sepeda;
+    private TextView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
         div_sepeda = findViewById(R.id.list_sepeda);
 //        div_sepeda.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -29,5 +41,20 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         finish();
+    }
+    private void logout() {
+        new AlertDialog.Builder(DashboardActivity.this)
+                .setTitle("Logout")
+                .setMessage("Anda yakin akan logout ?")
+                .setNegativeButton("Tidak", null)
+                .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    private void doNothing() {
+
+                    }
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Config.forceLogout(DashboardActivity.this);
+                    }
+                }).create().show();
     }
 }
